3ffb9bde9d8de69442d92651017b5ae1d5794f34
Sai Deep Konduri <s544028@nwmissouri.edu>
2021-11-18 21:10:15 -0600

    lab 13

--- app.js
+++ app.js
@@ -11,9 +11,23 @@ const resoureRouter = require('./routes/resource')
 const addModRouter = require('./routes/addmods');
 const selectorRouter = require('./routes/selector');
 const Costume = require("./models/costume");
-const dcComicsRouter = require("./routes/dccomics")
+const dcComicsRouter = require("./routes/dccomics");
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
 var app = express();
- 
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  }))
 const connectionString =  process.env.MONGO_CON;
 console.log(connectionString)
 mongoose.connect(connectionString,{useNewUrlParser: true, useUnifiedTopology: true}); 
@@ -33,6 +47,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
